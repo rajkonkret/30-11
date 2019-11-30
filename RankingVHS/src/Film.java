9bb4b0582ae4fdf72a7536dce4e6419627892a43
rajkonkret <rajkonkret660@gmail.com>
2019-11-30 17:58:22 +0100

    advisor

--- RankingVHS/src/Film.java
+++ RankingVHS/src/Film.java
@@ -3,17 +3,20 @@ class Film {
     private double ranking;
     private double imdb;
     private FilmCategory filmCategory;
+    private boolean ispresent;
 
     Film(
             String title,
             double ranking,
             double imdb,
-            FilmCategory filmCategory
+            FilmCategory filmCategory,
+            boolean ispresent
     ) {
         this.title = title;
         this.ranking = ranking;
         this.imdb = imdb;
         this.filmCategory = filmCategory;
+        this.ispresent = ispresent;
     }
 
     double getRanking() {
@@ -26,6 +29,10 @@ class Film {
 
 
     String toPrint() {
-        return title + " " + (int) ((ranking + imdb) / 2 * 100) + " pkt " + filmCategory;
+        return title + " " + (int) ((ranking + imdb) / 2 * 100) + " pkt " + filmCategory+" "+ispresent;
+    }
+
+    public void setIspresent(boolean ispresent) {
+        this.ispresent = ispresent;
     }
 }
--- RankingVHS/src/Main.java
+++ RankingVHS/src/Main.java
@@ -1,14 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Film dogs = new Film("Psy", 0.2, 0.5, FilmCategory.ACTION, true);
+        Film earth = new Film("Ziemia obiecana", 0.76, 0.99, FilmCategory.DRAMA, true);
+        Film batman = new Film("Batman", 0.99, 0.45, FilmCategory.COMEDY, true);
+        Film grey = new Film("50 Fades of Gray", 0.99, 0.99, FilmCategory.ACTION, true);
+        Film[] filmsToOffer = {dogs, earth, batman, grey};
 
-        Film dogs = new Film("Psy", 0.2, 0.5, FilmCategory.ACTION);
-        Film earth = new Film("Ziemia obiecana", 0.76, 0.99, FilmCategory.DRAMA);
-        Film batman = new Film("Batman", 0.99, 0.45, FilmCategory.COMEDY);
-        Film[] filmsToOffer = {dogs, earth, batman};
-
+        for (Film film : filmsToOffer) {
+            System.out.println(film.toPrint());
+        }
         Store advisorRadek = new Store(filmsToOffer);
-        Advisor advisor=new Advisor(advisorRadek,1,1);
-        System.out.println(advisor.findBestFromBoth().toPrint());
+        Advisor advisor = new Advisor(advisorRadek, 1, 1);
+        System.out.println("Polecany film: " + advisor.findBestFromBoth().toPrint());
 
+        System.out.println("Podaj film, który wypożyczasz");
+        int filmToRent = scanner.nextInt();
+        filmsToOffer[filmToRent - 1].setIspresent(false);
+        Film yourFilm = filmsToOffer[filmToRent - 1];
+        System.out.println("Wypożyczyłeś: " + yourFilm.toPrint());
+        System.out.println("Chcesz oddac? (t/n)");
+        String yourchoice = scanner.next();
+        if (yourchoice.equalsIgnoreCase("t")) {
+            yourFilm.setIspresent(true);
+            for (Film film : filmsToOffer) {
+                System.out.println(film.toPrint());
+            }
+        }
     }
 }
