d0e098de0acb4bde0e77e6089f93bf58315fa739
rajkonkret <rajkonkret660@gmail.com>
2019-11-30 13:28:43 +0100

    projekt VHS

--- src/ex2i3/Run.java
+++ src/ex2i3/Run.java
@@ -2,6 +2,8 @@ package ex2i3;
 
 public class Run {
     public static void main(String[] args) {
+        Tire celysee = new Tire(186, 65, 15);
+        System.out.println(celysee.toPrint());
 
     }
 }
---
+++ src/ex2i3/TireStorage.java
@@ -0,0 +1,10 @@
+package ex2i3;
+
+public class TireStorage {
+    private Tire[][] storage;
+
+    TireStorage(Tire[][] storage) {
+        this.storage = storage;
+    }
+
+}
